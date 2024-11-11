e0b900a990e0e96fc4ad3c3736d99a2cdcb3fcf8
Matheus Torres <torresmiranda.rj@gmail.com>
2024-11-11 11:57:28 -0300

    test: ensure SUT creates a new value object

--- tests/__unit__/modules/users/domain/value-objects/user-tokens.spec.ts
+++ tests/__unit__/modules/users/domain/value-objects/user-tokens.spec.ts
@@ -1,3 +1,5 @@
+import { userDefaultTokensAmount } from '@/@core/domain/constants/user-tokens';
+
 import { UserTokensValueObject } from '@/modules/users/domain/value-objects/tokens';
 
 describe(UserTokensValueObject.name, () => {
@@ -5,5 +7,10 @@ describe(UserTokensValueObject.name, () => {
 		expect(
 			() => new UserTokensValueObject('10000' as unknown as number),
 		).toThrow('Argument {amount} is required and must be a positive number.');
+	});
+
+	it('should create a new value object', () => {
+		const tokens = new UserTokensValueObject(userDefaultTokensAmount);
+		expect(tokens.amount).toEqual(1_000);
 	});
 });
