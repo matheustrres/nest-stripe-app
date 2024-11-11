2b61c88a5319c3d2eab803cb37fd267697fbce73
Matheus Torres <torresmiranda.rj@gmail.com>
2024-11-11 10:33:28 -0300

    test: ensure SUT restores a subscription

--- tests/__unit__/modules/subscriptions/domain/subscription.entity.spec.ts
+++ tests/__unit__/modules/subscriptions/domain/subscription.entity.spec.ts
@@ -1,3 +1,5 @@
+import { EntityCuid } from '@/@core/domain/entity-cuid';
+
 import { SubscriptionStatusEnum } from '@/modules/subscriptions/domain/enums/subscription-status';
 import { SubscriptionEntity } from '@/modules/subscriptions/domain/subscription.entity';
 
@@ -21,5 +23,20 @@ describe(SubscriptionEntity.name, () => {
 		expect(status).toBe(subscriptionStatus);
 		expect(status.isActive()).toBe(true);
 		expect(vendorCustomerId).toBe('customer_id');
+	});
+
+	it('should restore a subscription', () => {
+		const userId = new EntityCuid();
+		const subscription = new SubscriptionEntityBuilder()
+			.setUserId(userId)
+			.build();
+		const restoredSubscription = SubscriptionEntity.restore({
+			id: subscription.id,
+			props: subscription.getProps(),
+			createdAt: subscription.createdAt,
+		});
+
+		expect(subscription).toStrictEqual(restoredSubscription);
+		expect(restoredSubscription.getProps().userId).toStrictEqual(userId);
 	});
 });
