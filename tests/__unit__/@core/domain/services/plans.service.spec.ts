1947bae883632b16f74c271821abfb41cebf263c
Matheus Torres <matheustrres.rj@gmail.com>
2024-11-12 15:45:29 -0300

    test: ensure SUT returns the proper plan level when looking up for a plan level

--- tests/__unit__/@core/domain/services/plans.service.spec.ts
+++ tests/__unit__/@core/domain/services/plans.service.spec.ts
@@ -1,4 +1,5 @@
 import { CorePlansDomainService } from '@/@core/domain/services/plans.service';
+import { VendorPlanNameEnum } from '@/@core/enums/vendor-plan';
 
 describe(CorePlansDomainService.name, () => {
 	let service: CorePlansDomainService;
@@ -7,12 +8,25 @@ describe(CorePlansDomainService.name, () => {
 		service = new CorePlansDomainService();
 	});
 
-	describe('when looking up for a plan level', () => {
-		it('should return null for an invalid plan name', () => {
-			const planLevelFindingResult = service.getPlanLevel('Pr0fess1ion4l');
+	describe('.getPlanLevel()', () => {
+		describe('when looking up for a plan level', () => {
+			it('should return null for an invalid plan name', () => {
+				const planLevelFindingResult = service.getPlanLevel('Pr0fess1ion4l');
 
-			expect(planLevelFindingResult.isLeft()).toBe(true);
-			expect(planLevelFindingResult.value).toBe(null);
+				expect(planLevelFindingResult.isLeft()).toBe(true);
+				expect(planLevelFindingResult.isRight()).toBe(false);
+				expect(planLevelFindingResult.value).toBe(null);
+			});
+
+			it('should return the plan level', () => {
+				const planLevelFindingResult = service.getPlanLevel(
+					VendorPlanNameEnum.EssentialMonthly,
+				);
+
+				expect(planLevelFindingResult.isLeft()).toBe(false);
+				expect(planLevelFindingResult.isRight()).toBe(true);
+				expect(planLevelFindingResult.value).toBe('Essential');
+			});
 		});
 	});
 });
