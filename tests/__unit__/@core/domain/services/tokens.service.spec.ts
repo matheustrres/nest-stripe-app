950937062cec3ccd7521e75adf75b45549c44f54
Matheus Torres <matheustrres.rj@gmail.com>
2024-11-12 15:58:06 -0300

    test: ensure SUT returns an error if an invalid plan name is provided when handling plan tokens

---
+++ tests/__unit__/@core/domain/services/tokens.service.spec.ts
@@ -0,0 +1,27 @@
+import { InvalidPlanDomainError } from '@/@core/domain/errors/invalid-plan.error';
+import { CoreTokensDomainService } from '@/@core/domain/services/tokens.service';
+import { VendorPlanNameEnum } from '@/@core/enums/vendor-plan';
+
+describe(CoreTokensDomainService.name, () => {
+	let service: CoreTokensDomainService;
+
+	beforeEach(() => {
+		service = new CoreTokensDomainService();
+	});
+
+	describe('.handleTokensByPlan', () => {
+		describe('when handling plan tokens', () => {
+			it('should return an error if an invalid plan name is provided', () => {
+				const handlingTokensResult = service.handleTokensByPlan(
+					'Ess3nt1al' as VendorPlanNameEnum,
+				);
+
+				expect(handlingTokensResult.isLeft()).toBe(true);
+				expect(handlingTokensResult.isRight()).toBe(false);
+				expect(handlingTokensResult.value).toStrictEqual(
+					new InvalidPlanDomainError(),
+				);
+			});
+		});
+	});
+});
