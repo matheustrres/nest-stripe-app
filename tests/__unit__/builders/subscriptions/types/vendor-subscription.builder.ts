5a30c9ffca189ceaebe3fb5424d6c1966ac0eb1f
Matheus Torres <matheustrres.rj@gmail.com>
2024-11-13 20:23:38 -0300

    test: ensure SUT creates a subscription

---
+++ tests/__unit__/builders/subscriptions/types/vendor-subscription.builder.ts
@@ -0,0 +1,81 @@
+import { faker } from '@faker-js/faker';
+
+import { VendorCustomerBuilder } from './vendor-customer.builder';
+
+import {
+	VendorSubscriptionStatusEnum,
+	VendorSubscriptionType,
+} from '@/modules/subscriptions/application/clients/payments/payments.client';
+
+export class VendorSubscriptionBuilder {
+	#props: VendorSubscriptionType = {
+		id: faker.string.ulid(),
+		cancelAt: Date.now(),
+		cancelAtPeriodEnd: faker.datatype.boolean(),
+		currency: 'US',
+		currentPeriodEnd: Date.now(),
+		currentPeriodStart: Date.now(),
+		latestInvoice: 'in_1MowQWLkdIwHu7ixuzkSPfKd',
+		status: VendorSubscriptionStatusEnum.Active,
+		customer: new VendorCustomerBuilder().getProps().id,
+		created: Date.now(),
+	};
+
+	getProps(): VendorSubscriptionType {
+		return this.#props;
+	}
+
+	setId(id: string): this {
+		this.#props.id = id;
+		return this;
+	}
+
+	setCancelAt(cancelAt: number): this {
+		this.#props.cancelAt = cancelAt;
+		return this;
+	}
+
+	setCancelAtPeriodEnd(cancelAtPeriodEnd: boolean): this {
+		this.#props.cancelAtPeriodEnd = cancelAtPeriodEnd;
+		return this;
+	}
+
+	setCurrency(currency: string): this {
+		this.#props.currency = currency;
+		return this;
+	}
+
+	setCurrentPeriodStart(start: number): this {
+		this.#props.currentPeriodStart = start;
+		return this;
+	}
+
+	setCurrentPeriodEnd(end: number): this {
+		this.#props.currentPeriodEnd = end;
+		return this;
+	}
+
+	setLatestInvoice(latestInvoice: string): this {
+		this.#props.latestInvoice = latestInvoice;
+		return this;
+	}
+
+	setStatus(status: VendorSubscriptionStatusEnum): this {
+		this.#props.status = status;
+		return this;
+	}
+
+	setCustomerId(customerId: string): this {
+		this.#props.customer = customerId;
+		return this;
+	}
+
+	setCreated(created: number): this {
+		this.#props.created = created;
+		return this;
+	}
+
+	build(): VendorSubscriptionType {
+		return this.#props;
+	}
+}
--- tests/__unit__/modules/subscriptions/application/use-cases/create-subscription.use-case.spec.ts
+++ tests/__unit__/modules/subscriptions/application/use-cases/create-subscription.use-case.spec.ts
@@ -1,9 +1,16 @@
 import { Test } from '@nestjs/testing';
 
 import { InvalidCredentialsError } from '@/@core/application/errors/invalid-credentials.error';
+import { ExecutiveAnnualTokens } from '@/@core/domain/constants/tokens-per-plan';
+import { userDefaultTokensAmount } from '@/@core/domain/constants/user-tokens';
 import { left, right } from '@/@core/domain/logic/either';
 import { CorePlansDomainService } from '@/@core/domain/services/vendor-plans.service';
 import { CoreTokensDomainService } from '@/@core/domain/services/vendor-tokens.service';
+import {
+	VendorPlanFrequencyEnum,
+	VendorPlanLevelEnum,
+	VendorPlanNameEnum,
+} from '@/@core/enums/vendor-plan';
 
 import { VendorPaymentsClient } from '@/modules/subscriptions/application/clients/payments/payments.client';
 import { InvalidVendorSubscriptionActionError } from '@/modules/subscriptions/application/errors/invalid-vendor-subscription-action.error';
@@ -16,6 +23,7 @@ import { SubscriptionEntityBuilder } from '#/__unit__/builders/subscriptions/sub
 import { VendorCustomerBuilder } from '#/__unit__/builders/subscriptions/types/vendor-customer.builder';
 import { VendorPaymentMethodBuilder } from '#/__unit__/builders/subscriptions/types/vendor-payment-method.builder';
 import { VendorPlanBuilder } from '#/__unit__/builders/subscriptions/types/vendor-plan.builder';
+import { VendorSubscriptionBuilder } from '#/__unit__/builders/subscriptions/types/vendor-subscription.builder';
 import { CreateSubscriptionUseCaseBuilder } from '#/__unit__/builders/subscriptions/use-cases/create-subscription.builder';
 import { UserEntityBuilder } from '#/__unit__/builders/users/user.builder';
 
@@ -312,5 +320,90 @@ describe(CreateSubscriptionUseCase.name, () => {
 		expect(vendorPaymentsClient.customers.delete).toHaveBeenCalledWith(
 			customerId,
 		);
+	});
+
+	it('should create a subscription', async () => {
+		const user = new UserEntityBuilder()
+			.setTokens(userDefaultTokensAmount)
+			.build();
+		const vendorPlan = new VendorPlanBuilder()
+			.setName(VendorPlanNameEnum.ExecutiveAnnual)
+			.setLevel(VendorPlanLevelEnum.Executive)
+			.setFrequency(VendorPlanFrequencyEnum.Annual)
+			.build();
+		const vendorPM = new VendorPaymentMethodBuilder().build();
+		const { email, tokens } = user.getProps();
+		const vendorCustomer = new VendorCustomerBuilder().setEmail(email).build();
+		const vendorSubscription = new VendorSubscriptionBuilder()
+			.setCustomerId(vendorCustomer.id)
+			.build();
+
+		jest.spyOn(usersRepository, 'findOne').mockResolvedValueOnce(user);
+		jest
+			.spyOn(subscriptionsRepository, 'findByUserId')
+			.mockResolvedValueOnce(null);
+		jest
+			.spyOn(corePlansDomainService, 'getPlanByProductId')
+			.mockReturnValueOnce(right(vendorPlan));
+		jest
+			.spyOn(vendorPaymentsClient.paymentMethods, 'findById')
+			.mockResolvedValueOnce(right(vendorPM));
+		jest
+			.spyOn(vendorPaymentsClient.customers, 'create')
+			.mockResolvedValueOnce(right(vendorCustomer));
+		jest.spyOn(vendorPaymentsClient.customers, 'delete');
+		jest
+			.spyOn(vendorPaymentsClient.subscriptions, 'create')
+			.mockResolvedValueOnce(right(vendorSubscription));
+		jest
+			.spyOn(coreTokensDomainService, 'handleTokensByPlan')
+			.mockReturnValueOnce(right(ExecutiveAnnualTokens));
+		jest.spyOn(usersRepository, 'update');
+		jest.spyOn(subscriptionsRepository, 'insert');
+
+		const productId = vendorPlan.prodId;
+		const paymentMethodId = vendorPM.id;
+		const customerId = vendorCustomer.id;
+
+		const input = new CreateSubscriptionUseCaseBuilder()
+			.setUserId(user.id)
+			.setProductId(productId)
+			.setPaymentMethodId(paymentMethodId)
+			.getInput();
+
+		const currentUserTokensAmount = tokens.amount;
+		expect(currentUserTokensAmount).toEqual(userDefaultTokensAmount);
+
+		const { subscription } = await sut.exec(input);
+
+		const newUserTokensAmount = user.getProps().tokens.amount;
+
+		expect(subscription).toBeDefined();
+		expect(usersRepository.findOne).toHaveBeenCalledWith(input.userId);
+		expect(subscriptionsRepository.findByUserId).toHaveBeenCalledWith(
+			input.userId,
+		);
+		expect(corePlansDomainService.getPlanByProductId).toHaveBeenCalledWith(
+			productId,
+		);
+		expect(vendorPaymentsClient.paymentMethods.findById).toHaveBeenCalledWith(
+			paymentMethodId,
+		);
+		expect(vendorPaymentsClient.customers.create).toHaveBeenCalledWith(
+			email,
+			paymentMethodId,
+		);
+		expect(vendorPaymentsClient.subscriptions.create).toHaveBeenCalledWith(
+			customerId,
+			vendorPlan.priceId,
+			paymentMethodId,
+		);
+		expect(vendorPaymentsClient.customers.delete).not.toHaveBeenCalledWith();
+		expect(subscriptionsRepository.insert).toHaveBeenCalled();
+		expect(coreTokensDomainService.handleTokensByPlan).toHaveBeenCalledWith(
+			vendorPlan.name,
+		);
+		expect(usersRepository.update).toHaveBeenCalled();
+		expect(newUserTokensAmount).toEqual(ExecutiveAnnualTokens);
 	});
 });
