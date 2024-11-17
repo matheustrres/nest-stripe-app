07e4dfc61c70f1d0d3ec2a73b9f1a0121bba9542
Matheus Torres <matheustrres.rj@gmail.com>
2024-11-17 19:30:17 -0300

    test: ensure SUT throws a InvalidCredentialsError if given code does not match sign up code associated with given email

--- tests/__unit__/modules/users/application/use-cases/confirm-account.use-case.spec.ts
+++ tests/__unit__/modules/users/application/use-cases/confirm-account.use-case.spec.ts
@@ -2,11 +2,13 @@ import { Test } from '@nestjs/testing';
 
 import { InvalidCredentialsError } from '@/@core/application/errors/invalid-credentials.error';
 import { AlphanumericCodeService } from '@/@core/application/services/alpha-numeric-code.service';
+import { SignUpContextKey } from '@/@core/domain/constants/code-context';
 
 import { UsersRepository } from '@/modules/users/application/repositories/users.repository';
 import { ConfirmUserAccountUseCase } from '@/modules/users/application/use-cases/confirm-account.use-case';
 
 import { ConfirmUserAccountUseCaseBuilder } from '#/__unit__/builders/users/use-cases/confirm-account.builder';
+import { UserEntityBuilder } from '#/__unit__/builders/users/user.builder';
 
 describe(ConfirmUserAccountUseCase.name, () => {
 	let usersRepository: UsersRepository;
@@ -53,5 +55,30 @@ describe(ConfirmUserAccountUseCase.name, () => {
 			new InvalidCredentialsError(),
 		);
 		expect(usersRepository.findByEmail).toHaveBeenCalledWith(input.email);
+	});
+
+	it('should throw a InvalidCredentialsError if given {code} does not match sign up code associated with given {email}', async () => {
+		const user = new UserEntityBuilder().build();
+
+		jest.spyOn(usersRepository, 'findByEmail').mockResolvedValueOnce(user);
+		jest
+			.spyOn(alphanumericCodeService, 'validateCode')
+			.mockResolvedValueOnce(false);
+
+		const { email } = user.getProps();
+
+		const input = new ConfirmUserAccountUseCaseBuilder()
+			.setEmail(email)
+			.getInput();
+
+		await expect(sut.exec(input)).rejects.toThrow(
+			InvalidCredentialsError.byValidationCode(),
+		);
+		expect(usersRepository.findByEmail).toHaveBeenCalledWith(email);
+		expect(alphanumericCodeService.validateCode).toHaveBeenCalledWith(
+			SignUpContextKey,
+			email,
+			input.code,
+		);
 	});
 });
