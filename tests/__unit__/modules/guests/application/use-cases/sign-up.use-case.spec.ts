8611f540b6e3d65b5c8cd757305602e160d78b9d
Matheus Torres <matheustrres.rj@gmail.com>
2024-11-28 10:20:05 -0300

    test: ensure SUT throws a UserAlreadyExistsError if extracted email from decoded token is already in use

--- tests/__unit__/modules/guests/application/use-cases/sign-up.use-case.spec.ts
+++ tests/__unit__/modules/guests/application/use-cases/sign-up.use-case.spec.ts
@@ -9,10 +9,21 @@ import { GuestSignUpTokenSubType, JwtPayload } from '@/@core/types';
 import { GuestsRepository } from '@/modules/guests/application/repositories/guests.repository';
 import { InvitesRepository } from '@/modules/guests/application/repositories/invites.repository';
 import { GuestSignUpUseCase } from '@/modules/guests/application/use-cases/sign-up.use-case';
+import { UserAlreadyExistsError } from '@/modules/users/application/errors/user-already-exists.error';
 import { UsersRepository } from '@/modules/users/application/repositories/users.repository';
 import { UsersService } from '@/modules/users/application/services/users.service';
 
+import { InviteEntityBuilder } from '#/__unit__/builders/guests/invite.builder';
 import { GuestSignUpUseCaseBuilder } from '#/__unit__/builders/guests/use-cases/guest-sign-up.builder';
+import { UserEntityBuilder } from '#/__unit__/builders/users/user.builder';
+
+function bufferizeTokenSub(
+	ownerId: string,
+	inviteId: string,
+	guestEmail: string,
+): string {
+	return Buffer.from(`${ownerId}:${inviteId}:${guestEmail}`).toString('base64');
+}
 
 describe(GuestSignUpUseCase.name, () => {
 	let guestsRepository: GuestsRepository;
@@ -134,5 +145,41 @@ describe(GuestSignUpUseCase.name, () => {
 		);
 		expect(tokenizationService.verify).toHaveBeenCalledWith('alternative_jwt');
 		expect(tokenizationService.decode).toHaveBeenCalledWith('alternative_jwt');
+	});
+
+	it('should throw a UserAlreadyExistsError if extracted email from decoded token sub is already in use', async () => {
+		const commonEmail = 'john.doe@gmail.com';
+
+		const user = new UserEntityBuilder().setEmail(commonEmail).build();
+		const owner = new UserEntityBuilder().build();
+		const guest = new UserEntityBuilder()
+			.setEmail(user.getProps().email)
+			.build();
+		const invite = new InviteEntityBuilder()
+			.setGuestId(guest.id)
+			.setOwnerId(owner.id)
+			.build();
+
+		jest.spyOn(tokenizationService, 'verify').mockReturnValueOnce(true);
+		jest.spyOn(tokenizationService, 'decode').mockReturnValueOnce({
+			role: RoleEnum.Guest,
+			sub: bufferizeTokenSub(
+				owner.id.value,
+				invite.id.value,
+				guest.getProps().email,
+			),
+		} as JwtPayload);
+		jest.spyOn(usersRepository, 'findByEmail').mockResolvedValueOnce(user);
+
+		const input = new GuestSignUpUseCaseBuilder()
+			.setToken('alternative_jwt')
+			.getInput();
+
+		await expect(sut.exec(input)).rejects.toThrow(
+			UserAlreadyExistsError.byEmail(commonEmail),
+		);
+		expect(tokenizationService.verify).toHaveBeenCalledWith('alternative_jwt');
+		expect(tokenizationService.decode).toHaveBeenCalledWith('alternative_jwt');
+		expect(usersRepository.findByEmail).toHaveBeenCalledWith(commonEmail);
 	});
 });
