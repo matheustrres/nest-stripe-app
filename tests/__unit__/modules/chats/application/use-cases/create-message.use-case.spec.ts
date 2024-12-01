9b660d7c889c8f6aa9306bab342af581a6b7ba0c
Matheus Torres <matheustrres.rj@gmail.com>
2024-12-01 12:28:17 -0300

    test: ensure SUT throws a ChatNotFoundError if no chat is found with given chatId

--- tests/__unit__/modules/chats/application/use-cases/create-message.use-case.spec.ts
+++ tests/__unit__/modules/chats/application/use-cases/create-message.use-case.spec.ts
@@ -2,12 +2,14 @@ import { Test } from '@nestjs/testing';
 
 import { InvalidCredentialsError } from '@/@core/application/errors/invalid-credentials.error';
 
+import { ChatNotFoundError } from '@/modules/chats/application/errors/chat-not-found.error';
 import { ChatsRepository } from '@/modules/chats/application/repositories/chats.repository';
 import { MessagesRepository } from '@/modules/chats/application/repositories/messages.repository';
 import { CreateMessageUseCase } from '@/modules/chats/application/use-cases/create-message.use-case';
 import { UsersRepository } from '@/modules/users/application/repositories/users.repository';
 
 import { CreateMessageUseCaseBuilder } from '#/__unit__/builders/chats/use-cases/create-message.builder';
+import { UserEntityBuilder } from '#/__unit__/builders/users/user.builder';
 
 describe(CreateMessageUseCase.name, () => {
 	let usersRepository: UsersRepository;
@@ -62,5 +64,25 @@ describe(CreateMessageUseCase.name, () => {
 			new InvalidCredentialsError(),
 		);
 		expect(usersRepository.findById).toHaveBeenCalledWith(input.userId);
+	});
+
+	it('should throw a ChatNotFoundError if no chat is found with given {chatId}', async () => {
+		const user = new UserEntityBuilder().build();
+
+		jest.spyOn(usersRepository, 'findById').mockResolvedValueOnce(user);
+		jest.spyOn(chatsRepository, 'findByOwnerId').mockResolvedValueOnce(null);
+
+		const input = new CreateMessageUseCaseBuilder()
+			.setUserId(user.id)
+			.getInput();
+
+		await expect(sut.exec(input)).rejects.toThrow(
+			ChatNotFoundError.byId(input.chatId),
+		);
+		expect(usersRepository.findById).toHaveBeenCalledWith(input.userId);
+		expect(chatsRepository.findByOwnerId).toHaveBeenCalledWith(
+			input.chatId,
+			input.userId,
+		);
 	});
 });
